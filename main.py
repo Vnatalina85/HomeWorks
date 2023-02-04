9ccbfab8921f52a86d216e7796d843ebec372a13
Vnatalina85 <v.natalina@mail.ru>
2023-02-04 18:27:45 +0300

    HomeWork1

---
+++ main.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = 3
+    b = 4
+    c = 5
+    if a<b+c and b<a+c and c<a+b:
+        print('треугольник существует')
+    else:
+        print('треугольник не существует')
+
